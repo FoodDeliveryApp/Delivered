57acc3df4f9c3a659c06a47278faaec99c275d14
MarinaOrzechowski <43459295+MarinaOrzechowski@users.noreply.github.com>
2019-12-06 13:33:30 -0500

    delete repeating files

--- delivery_js/classes/bid.js
+++
@@ -1,9 +0,0 @@
-class Bid {
-    constructor(delivery_person, amount) {
-        this.delivery_person = delivery_person;
-        this.amount;
-    }
-    increase_amount(new_amount) { this.amount = new_amount };
-    remove_bid() { delete this };
-
-}
